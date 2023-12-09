7f29d9baf00b80b85afaaf79bb8c9efe33a7c7ee
SugarCube1337 <danila_pechkurov@mail.ru>
2023-12-09 03:46:02 +0300

    command pattern, help command

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ struct ThreadData_s {
 #define WORK_TIMEOUT \
     nanosleep(&ts, NULL);
 
+
 /*--------------*/
 
 // Child thread
@@ -36,6 +37,19 @@ int NeedQuit(pthread_mutex_t *mtx) {
             perror("Error locking mutex");
             exit(EXIT_FAILURE);
     }
+}
+
+// Command interface
+typedef struct {
+    void (*execute)(struct ThreadData_s *threadData);
+} Command;
+
+void exitCommand(struct ThreadData_s *threadData) {
+    kill(threadData->mainPid, SIGUSR1);
+}
+
+void helpCommand() {
+    printf("Available commands:\nhelp - shows information about commands\nexit - stops the program\n>");
 }
 
 int InputAvailable() {
@@ -63,18 +77,27 @@ void *Cli(void *arg) {
     setbuf(stdout, NULL);
     printf("> ");
 
+    // Define commands
+    Command exitCmd = {exitCommand};
+    Command helpCmd = {helpCommand};
+
     while (!NeedQuit(&threadData->stopper)) {
         if (InputAvailable()) {
             fgets(userInput, sizeof(userInput), stdin);
 
             if (memcmp(userInput, "exit", 4) == 0) {
-                kill(threadData->mainPid, SIGUSR1);
+                exitCmd.execute(threadData);
                 break;
-            } else if (memcmp(userInput, "\n", 1) == 0) {
+            } else if (memcmp(userInput, "help", 4) == 0){
+                helpCmd.execute(threadData);
+            }
+            else if (memcmp(userInput, "\n", 1) == 0) {
                 printf("> ");
                 memset(userInput, 0, sizeof(userInput));
-            } else {
+            }
+            else {
                 printf("Unknown command: %s", userInput);
+                printf("Type 'help' for available commands.\n");
                 printf("> ");
                 memset(userInput, 0, sizeof(userInput));
             }
