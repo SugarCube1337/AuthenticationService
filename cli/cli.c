69c6357d242091123e38177c580feab41185efa2
SugarCube1337 <danila_pechkurov@mail.ru>
2023-12-13 21:51:26 +0300

    commands update (user). TODO: update

--- cli/cli.c
+++ cli/cli.c
@@ -38,7 +38,7 @@ void AddService(struct ThreadData_s *threadData) {
     while (!NeedQuit(&threadData->stopper)) {
         if (InputAvailable()) {
             fgets(userInput, sizeof(userInput), stdin);
-            userInput[strlen(userInput)-1] = '\0'; // rem '\n'
+            userInput[strlen(userInput) - 1] = '\0'; // rem '\n'
             break;
         }
         WORK_TIMEOUT
@@ -61,12 +61,12 @@ void AddService(struct ThreadData_s *threadData) {
 void ShowServices(struct ThreadData_s *threadData) {
     const bson_t *foundDoc;
     bson_t *query = bson_new();
-    mongoc_cursor_t *cursor = mongoc_collection_find_with_opts (threadData->db.collServ, query, NULL, NULL);
+    mongoc_cursor_t *cursor = mongoc_collection_find_with_opts(threadData->db.collServ, query, NULL, NULL);
 
-    while (mongoc_cursor_next (cursor, &foundDoc)) {
-        char *str = bson_as_canonical_extended_json (foundDoc, NULL);
-        printf ("%s\n", str);
-        bson_free (str);
+    while (mongoc_cursor_next(cursor, &foundDoc)) {
+        char *str = bson_as_canonical_extended_json(foundDoc, NULL);
+        printf("%s\n", str);
+        bson_free(str);
     }
 
     bson_destroy(query);
@@ -84,7 +84,7 @@ void DelService(struct ThreadData_s *threadData) {
     while (!NeedQuit(&threadData->stopper)) {
         if (InputAvailable()) {
             fgets(userInput, sizeof(userInput), stdin);
-            userInput[strlen(userInput)-1] = '\0'; // rem '\n'
+            userInput[strlen(userInput) - 1] = '\0'; // rem '\n'
             break;
         }
         WORK_TIMEOUT
@@ -96,9 +96,9 @@ void DelService(struct ThreadData_s *threadData) {
     bson_oid_init_from_string(&oid, userInput);
 
     BSON_APPEND_OID (deletedDoc, "_id", &oid);
-    if (!mongoc_collection_delete_one (
+    if (!mongoc_collection_delete_one(
             threadData->db.collServ, deletedDoc, NULL, NULL, &error)) {
-        fprintf (stderr, "Delete failed: %s\n", error.message);
+        fprintf(stderr, "Delete failed: %s\n", error.message);
         exit(1);
     }
 
@@ -108,99 +108,151 @@ void DelService(struct ThreadData_s *threadData) {
 
 
 void addUser(struct ThreadData_s *threadData) {
+    bson_error_t error;
+    char userInput[1024];
+    memset(userInput, 0, sizeof(userInput));
+
+    SET_WORK_TIMEOUT
+
+    printf("Enter user name: ");
+    while (!NeedQuit(&threadData->stopper)) {
+        if (InputAvailable()) {
+            fgets(userInput, sizeof(userInput), stdin);
+            userInput[strlen(userInput)-1] = '\0'; // rem '\n'
+            break;
+        }
+        WORK_TIMEOUT
+    }
+    bson_t *createdUserDoc = bson_new();
+    bson_oid_t oid;
+    bson_oid_init(&oid, NULL);
+    BSON_APPEND_OID(createdUserDoc, "_id", &oid);
+    BSON_APPEND_UTF8(createdUserDoc, "username", userInput);
+
+    if (!mongoc_collection_insert_one(threadData->db.collUser, createdUserDoc, NULL, NULL, &error)) {
+        fprintf(stderr, "mongoc_collection_insert_one() : %s\n", error.message);
+    }
+
+    printf("Ok\n");
+    bson_destroy(createdUserDoc);
 }
 
 void updateUser(struct ThreadData_s *threadData){
 
 }
 
-void showUsers(struct ThreadData_s *threadData){
+void showUsers(struct ThreadData_s *threadData) {
+    const bson_t *foundDoc;
+    bson_t *query = bson_new();
+    mongoc_cursor_t *cursor = mongoc_collection_find_with_opts(threadData->db.collUser, query, NULL, NULL);
 
+    while (mongoc_cursor_next(cursor, &foundDoc)) {
+        char *str = bson_as_canonical_extended_json(foundDoc, NULL);
+        printf("%s\n", str);
+        bson_free(str);
+    }
+
+    bson_destroy(query);
+    mongoc_cursor_destroy(cursor);
 }
 
-void deleteUser(struct ThreadData_s *threadData){
+void deleteUser(struct ThreadData_s *threadData) {
+    bson_error_t error;
+    char userInput[1024];
+    memset(userInput, 0, sizeof(userInput));
 
+    SET_WORK_TIMEOUT
+
+    printf("Enter user OID: ");
+    while (!NeedQuit(&threadData->stopper)) {
+        if (InputAvailable()) {
+            fgets(userInput, sizeof(userInput), stdin);
+            userInput[strlen(userInput)-1] = '\0'; // rem '\n'
+            break;
+        }
+        WORK_TIMEOUT
+    }
+
+    bson_oid_t oid;
+    bson_t *deletedDoc = bson_new();
+
+    bson_oid_init_from_string(&oid, userInput);
+
+    BSON_APPEND_OID(deletedDoc, "_id", &oid);
+
+    if (!mongoc_collection_delete_one(
+            threadData->db.collUser, deletedDoc, NULL, NULL, &error)) {
+        fprintf(stderr, "Delete failed: %s\n", error.message);
+        exit(1);
+    }
+
+    printf("Ok\n");
+    bson_destroy(deletedDoc);
 }
 
 //---------------------------------------------------//
 
-void *Cli(void *arg) {
-    struct ThreadData_s *threadData = (struct ThreadData_s *) arg;
+    void *Cli(void *arg) {
+        struct ThreadData_s *threadData = (struct ThreadData_s *) arg;
 
-    SET_WORK_TIMEOUT
+        SET_WORK_TIMEOUT
 
-    char userInput[256];
-    memset(userInput, 0, sizeof(userInput));
+        char userInput[256];
+        memset(userInput, 0, sizeof(userInput));
 
-    // Define commands
-    Command exitCmd = {exitCommand};
-    Command helpCmd = {helpCommand};
-    Command addUserCmd = {addUser};
-    Command addServCmd = {AddService};
-    Command showServCmd = {ShowServices};
-    Command delServCmd = {DelService};
-    Command delUserCmd = {deleteUser};
+        // Define commands
+        Command exitCmd = {exitCommand};
+        Command helpCmd = {helpCommand};
+        Command addUserCmd = {addUser};
+        Command addServCmd = {AddService};
+        Command showServCmd = {ShowServices};
+        Command delServCmd = {DelService};
+        Command delUserCmd = {deleteUser};
+        Command showUsersCmd = {showUsers};
+        Command updateUserCmd = {updateUser};
 
-    setbuf(stdout, NULL);
-    printf("Type 'help' for available commands.\n");
-    printf("> ");
+        setbuf(stdout, NULL);
+        printf("Type 'help' for available commands.\n");
+        printf("> ");
 
-    while (!NeedQuit(&threadData->stopper)) {
-        if (InputAvailable()) {
-            fgets(userInput, sizeof(userInput), stdin);
+        while (!NeedQuit(&threadData->stopper)) {
+            if (InputAvailable()) {
+                fgets(userInput, sizeof(userInput), stdin);
 
-            if (memcmp(userInput, "\n", 1) == 0) {
-                //printf("> ");
+                if (memcmp(userInput, "\n", 1) == 0) {
+                    //printf("> ");
+                } else if (memcmp(userInput, "exit", 4) == 0) {
+                    exitCmd.execute(threadData);
+                    break;
+                } else if (memcmp(userInput, "help", 4) == 0) {
+                    helpCmd.execute(threadData);
+                } else if (memcmp(userInput, "adduser", 7) == 0) {
+                    addUserCmd.execute(threadData);
+                } else if (memcmp(userInput, "updateuser", 8) == 0) {
+                    updateUserCmd.execute(threadData);
+                } else if (memcmp(userInput, "deluser", 7) == 0) {
+                    delUserCmd.execute(threadData);
+                } else if (memcmp(userInput, "addservice", 10) == 0) {
+                    addServCmd.execute(threadData);
+                } else if (memcmp(userInput, "delservice", 10) == 0) {
+                    delServCmd.execute(threadData);
+                } else if (memcmp(userInput, "showusers", 9) == 0) {
+                    showUsersCmd.execute(threadData);
+                } else if (memcmp(userInput, "showservices", 12) == 0) {
+                    showServCmd.execute(threadData);
+                } else {
+                    printf("Unknown command: %s", userInput);
+                    printf("Type 'help' for available commands.\n");
+
+                }
+                printf("> ");
+                memset(userInput, 0, sizeof(userInput));
             }
 
-            else if (memcmp(userInput, "exit", 4) == 0) {
-                exitCmd.execute(threadData);
-                break;
-            }
-
-            else if (memcmp(userInput, "help", 4) == 0){
-                helpCmd.execute(threadData);
-            }
-
-            else if (memcmp(userInput, "adduser", 7) == 0) {
-                addUserCmd.execute(threadData);
-            }
-
-            //else if (memcmp(userInput, "updateuser", 8) == 0) {
-            //}
-
-            else if (memcmp(userInput, "deluser", 7) == 0) {
-                delUserCmd.execute(threadData);
-            }
-
-            else if (memcmp(userInput, "addservice", 10) == 0) {
-                addServCmd.execute(threadData);
-            }
-
-            else if (memcmp(userInput, "delservice", 10) == 0) {
-                delServCmd.execute(threadData);
-            }
-
-            else if (memcmp(userInput, "showusers", 9) == 0) {
-                showServCmd.execute(threadData);
-            }
-
-            else if (memcmp(userInput, "showservices", 12) == 0){
-                showServCmd.execute(threadData);
-            }
-            else {
-                printf("Unknown command: %s", userInput);
-                printf("Type 'help' for available commands.\n");
-
-            }
-            printf("> ");
-            memset(userInput, 0, sizeof(userInput));
+            WORK_TIMEOUT
         }
 
-        WORK_TIMEOUT
+        // Прощальное сообщение
+        printf("Child thread says goodbye!\n");
+        return NULL;
     }
-
-    // Прощальное сообщение
-    printf("Child thread says goodbye!\n");
-    return NULL;
-}
